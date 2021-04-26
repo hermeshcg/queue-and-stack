041456e19864fe784e1d7b84f6f2da298f9be005
Hermes Caretta <hermescaretta@gmail.com>
2021-04-26 11:03:32 -0300

    queue and stack

--- index.js
+++ index.js
@@ -14,4 +14,57 @@ function mult(x, y) {
   return x * y;
 }
 
-module.exports = { sum, sub, mult, div };
+function Queue() {
+  const data = [];
+
+  const add = (value) => {
+    data.push(value);
+  };
+
+  const remove = () => {
+    if (data.length === 0) {
+      return -1;
+    }
+    const value = data[data.length - 1];
+    data.splice(data.length - 1, 1);
+    return value;
+  };
+
+  const print = () => {
+    console.log(data);
+  };
+
+  return {
+    add,
+    remove,
+    print,
+  };
+}
+
+class Pilha {
+  constructor() {
+    var items = [];
+
+    this.empilha = function (element) {
+      items.push(element);
+    };
+
+    this.desempilha = function () {
+      return items.pop();
+    };
+
+    this.isEmpty = function () {
+      return items.length === 0;
+    };
+
+    this.size = function () {
+      return items.length;
+    };
+
+    this.print = function () {
+      console.log(items.toString());
+    };
+  }
+}
+
+module.exports = { sum, sub, mult, div, Queue, Pilha };
